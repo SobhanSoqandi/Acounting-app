85e0fbc5a246a66bb115b40a507141ff360ae6c9
SobhanSoqandi <sobhansoqandi@gmail.com>
2025-03-18 15:09:07 +0330

    two commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  const [ number , setNumber ] = useState(1);
+
+  return (
+    <>
+  
+
+<h1> Hellow Worldddd </h1>
+  
+    </>
+  )
+}
+
+export default App
